edf436c996e8a6d2221fb8419270808626b67554
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-15 06:11:59 -0700

    mark

---
+++ marks.js
@@ -0,0 +1,12 @@
+let mark = prompt("Enter a number");
+if(mark>=90 && mark<=100){
+ console.log("A")
+}else if(mark>=70 && mark<=89){
+ console.log("B")
+}else if(mark>=60 && mark<=69){
+console.log("C");
+} else if(mark>=50 && mark<=59){
+console.log("D");
+}else{
+    console.log("Fail");
+}
