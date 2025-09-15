f2552e72fc06d4902867d1056c6cfc577cb1bf57
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-15 06:10:43 -0700

    add operator

---
+++ operator.js
@@ -0,0 +1,11 @@
+let a=5;
+let b=2;
+console.log("a and b =",a+b);
+let c=5;
+  console.log(c>=5? "yes":"no");
+let num = prompt("enter number");
+if(num%5 == 0){
+console.log("it is a multiple of 5")
+}else{
+console.log("it is not a multiple of 5")
+}
