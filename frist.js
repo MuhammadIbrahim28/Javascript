db012f65bfb7e5c5df9b11ff4951b54ab5949587
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-14 21:45:54 -0700

    add frist js

---
+++ frist.js
@@ -0,0 +1,31 @@
+/*var age =20;
+var age =30;
+var age = 80;
+console.log(age);
+*/
+/*let name = "Muhammad Ibrahim";
+//let name = "ibrahim";
+name= "ibrahim";
+console.log(name);
+*/
+/*const name ="ibrahim";
+    // name ="ibrahim"
+     console.log(name);
+     */
+    /*const student ={
+        name: "ibrahim",
+        age:27,
+        add:"karachi"
+
+    };
+    student.name = "Muhammad ibrahim";
+    console.log(student);
+    console.log(student.name)
+    */
+   const user ={
+        name: "muhammad ibrahim",
+        post: 100,
+        isFollow: 101,
+        isFollower :202
+   }
+   console.log(user)
