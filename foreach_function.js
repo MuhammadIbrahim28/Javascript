7fe7b4d4dfcd5ca387ef4d3bfb6a8276cdd0a606
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-16 11:06:13 -0700

    foreach

---
+++ foreach_function.js
@@ -0,0 +1,11 @@
+/*let cities = ["ukot","mithi","jamshoro"];
+cities.forEach((val) =>{
+  console.log(val);
+});
+*/
+   /*let arr = [1,2,4,5,6];
+   arr.forEach((val) =>{
+    console.log(val);
+   }
+   )*/
+   
