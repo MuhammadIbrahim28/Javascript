080a74dc10aacc73be5ebed571b9af7c25f1cef5
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-17 06:02:38 -0700

    find

---
+++ find_findindex.js
@@ -0,0 +1,9 @@
+let data = [10,20,7,50,1,100];
+ /* let result = data.find((item) =>{
+        return item < 50;
+ })
+ console.log(result); */
+ let result = data.findIndex((item) =>{
+        return item < 50;
+ })
+ console.log(result);
