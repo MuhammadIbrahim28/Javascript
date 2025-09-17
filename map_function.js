c1d51636fe8e777c7b227c2b21c0d730a759b952
MuhammadIbrahim28 <mibrahim17sw28@gmail.com>
2025-09-17 05:05:15 -0700

    map

---
+++ map_function.js
@@ -0,0 +1,11 @@
+// value return
+/* let nums = [2,3,4,5];
+nums.map((val)=>{
+    console.log(val);
+});
+*/
+let nums = [2,3,4,5];
+ let newarr = nums.map((val)=>{
+    return val*val;
+});
+console.log(newarr);
